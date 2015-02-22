504c73e0fbe7c9b19c1b5858f5974ef8c9121c41
Diego Martín <dmsgago@gmail.com>
2015-02-22 14:04:55 +0100

    He añadido un buble que recorre los puntos limpios y genera una URL con su ubicacion en OpenStreetMap

--- puntoslimpios.py
+++ puntoslimpios.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #Diego Martín Sánchez 1ºASIR, ejercicio_json
+
+#Funciones
 import json
+
+#Variables
 f=open("ecopuntos.json","r")
 docu=json.load(f)
 elemento=''
@@ -13,5 +17,12 @@ for punto in docu["features"]:
     elemento=punto["properties"]["elemento"]
     direccion=punto["properties"]["direccion"]
     print("\nElemento: %s\nDireccion: %s"%(elemento, direccion))
+
+#Lista los puntos limpios y genera un enlace con su ubicacion en OpenStreetMap
+for punto in docu["features"]:
+    elemento=punto["properties"]["elemento"]
+    latitud=punto["properties"]["latitud"]
+    longitud=punto["properties"]["longitud"]
+    print("\nElemento: %s\nUbicacion: http://www.openstreetmap.org/#map=15/%s/%s"%(elemento, latitud, longitud))
 
 f.close
