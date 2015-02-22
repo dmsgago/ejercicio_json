d817c46608e2326bb4a5ca01ea8022ba0a8fc509
Diego Martín <dmsgago@gmail.com>
2015-02-22 13:09:04 +0100

    He añadido un bucle que recorre el fichero .json y extrae de cada punto limpio su dirección

--- puntoslimpios.py
+++ puntoslimpios.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #Diego Martín Sánchez 1ºASIR, ejercicio_json
+import json
+f=open("ecopuntos.json","r")
+docu=json.load(f)
+elemento=''
+direccion=''
+
+#Listar todas las direcciones de los puntos limpios de Málaga
+print("\nDirecciones de puntos limpios:")
+for punto in docu["features"]:
+    elemento=punto["properties"]["elemento"]
+    direccion=punto["properties"]["direccion"]
+    print("\nElemento: %s\nDireccion: %s"%(elemento, direccion))
+
+f.close
